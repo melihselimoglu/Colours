3ca2b000cd524c99462b74f411e6e4134af4c985
BuildTools <melihselimog_2007@hotmail.com>
2025-07-14 21:35:37 +0300

    import for components

--- src/main/java/com/deviro/maven/services/impl/EnglishRedPrinter.java
+++ src/main/java/com/deviro/maven/services/impl/EnglishRedPrinter.java
@@ -1,7 +1,9 @@
 package com.deviro.maven.services.impl;
 
 import com.deviro.maven.services.RedPrinter;
+import org.springframework.stereotype.Component;
 
+@Component
 public class EnglishRedPrinter implements RedPrinter {
 
   @Override
